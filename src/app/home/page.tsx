611ea1497724ec9caf12bd609c60c85b3950087a
Yash rai <yash2154rai@gmail.com>
2025-01-05 17:14:44 +0530

    add protect page feature

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,14 @@
 import HomePage from "./HomePage";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation"
+
 
 export default async function ProtectedHomePage() {
+  const session = await auth()
+  if (!session) { 
+
+    return redirect("/") 
+  }
   return <HomePage />
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster/>
       </body>
     </html>
   );
