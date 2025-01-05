2d99c3eb3a88de602dde5f05521c16644285fc27
Yash rai <yash2154rai@gmail.com>
2025-01-05 17:24:06 +0530

    fix bug

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,11 +2,9 @@ import HomePage from "./HomePage";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation"
 
-
 export default async function ProtectedHomePage() {
   const session = await auth()
   if (!session) { 
-
     return redirect("/") 
   }
   return <HomePage />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +36,6 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
-        <Toaster/>
       </body>
     </html>
   );
