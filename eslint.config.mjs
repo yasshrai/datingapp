9e3d26bce02d3bc66aca37457bf9cda24c23fc6a
Yash rai <yash2154rai@gmail.com>
2025-01-07 23:42:28 +0530

    change in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript",),
   ...compat.config({
     extends: ['next'],
     rules: {
